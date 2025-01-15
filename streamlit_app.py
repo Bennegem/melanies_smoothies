927d34a01a8f85d2e930cafd9d7bc90547613fb0
Bennegem <be.meissner13@gmail.com>
2025-01-15 09:57:50 +0100

    Update streamlit_app.py
    search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ session = cnx.session()
 
 #session = get_active_session()
 #erstellt den Frame "my_dataframe"
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARC_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 #erstellt die auswahlliste, was eine Liste zusammenstellt
 ingredients_list=st.multiselect(
