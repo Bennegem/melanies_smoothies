41a6f724370841c91c6b3aefd14210d94c6db165
Bennegem <be.meissner13@gmail.com>
2025-01-16 08:02:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 # Import python packages
-#import streamlit as st
-#from snowflake.snowpark.functions import col
-#import requests
-#import pandas as pd
-
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+import pandas as pd
+-213
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
